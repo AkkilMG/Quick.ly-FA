6cd32db28a3651bd7eac572419c874a0598ea473
heimanpictures <heimanpicturesofficial@gmail.com>
2023-04-12 22:34:45 +0530

    Issue fixed

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ app.include_router(admin_router.router)
 
 # for local testing
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="localhost", port=10000, reload=True)
+  uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
